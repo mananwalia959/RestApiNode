f43951de15fa177eed4ae6ebb77482dda4337d08
mananwalia959 <mananwalia959@gmail.com>
2018-09-26 22:52:51 +0530

    Initial commit

---
+++ api/routes/products.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+const mongoose=require('mongoose');
+const Product = require('../models/products');
+
+
+
+router.get('/',(req,res,next)=>{
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs)
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
+    })
+   
+})
+router.post('/', (req, res, next) => {
+    const product= new Product({
+        _id:new mongoose.Types.ObjectId(),
+        name:req.body.name,
+        price:req.body.price
+    });
+    product.save()
+    .then(result =>{
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdproduct: product
+        });
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({
+            error:error
+        });
+    })
+    
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+    .exec()
+    .then(doc=>{
+        console.log("From datbase",doc);
+        if(doc) {
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404).json({
+                error:'No Product found for particuar id'
+            });
+
+        }
+        
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:err})
+    })
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id= req.params.productId;
+    const updateops ={};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Product.update({_id:id},{$set:updateops})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result)
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({error:err})
+    })
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id= req.params.productId;
+    Product.deleteOne({ _id: id })
+    .exec()
+    .then(doc=>{
+        res.status(200).json(doc);
+    })
+    .catch(err=>{
+    console.log(err);
+    res.status(500).json({error:err})
+});
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,52 @@
+const express= require('express');
+const app =express();
+const morgan = require('morgan');
+const bodyParser=require('body-parser');
+const mongoose =require('mongoose');
+var cors = require('cors');
+
+//importing routes
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+//Mongoose Database
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/restfulapi',
+)
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+
+
+//Routes used
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status=404;
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status|| 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+
+})
+
+
+
+//  app hosted
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
